bee17a21d04531fd2abcb97ec5a72808d4141bdb
Liam Cusack <LCusack8108@gmail.com>
2021-03-09 16:09:00 -0700

    Add flickr service, and retrieve images method. add backgrounds facade find_image method

--- app/facades/backgrounds_facade.rb
+++ app/facades/backgrounds_facade.rb
@@ -1,8 +1,12 @@
 class BackgroundsFacade
   class << self
 
-    def get_backgrounds(location)
-      
+    def get_background(location)
+      image = find_image(location)
+    end
+
+    def find_image(location)
+      images = FlickrService.retrieve_images(location)
     end
 
   end
