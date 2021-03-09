2b7120a14d33ad7cb98bbc90a101697c8bdcc781
Liam Cusack <LCusack8108@gmail.com>
2021-03-09 16:12:26 -0700

    add get_background, and find_images tests to background facade

--- spec/facades/backgrounds_facade_spec.rb
+++ spec/facades/backgrounds_facade_spec.rb
@@ -2,5 +2,13 @@ require 'rails_helper'
 
 describe BackgroundsFacade, :vcr do
   describe "class methods" do
+    before :each do
+    end
+
+    it "get_background" do
+    end
+
+    it "find_images" do
+    end
   end
 end
