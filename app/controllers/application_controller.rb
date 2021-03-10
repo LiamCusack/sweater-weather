9cbbe842472746ad7fb7689ffb9f68b3daa8a93c
Liam Cusack <LCusack8108@gmail.com>
2021-03-09 21:59:21 -0700

    Add sad path testing for user api and unique rescue messages in application record

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,20 @@
 class ApplicationController < ActionController::API
   rescue_from "Bad Request", with: :status_400
-  rescue_from ActiveRecord::RecordInvalid, with: :passwords_dont_match
+  rescue_from ActiveRecord::RecordInvalid, with: :status_400
 
-  def passwords_dont_match
-    render json: {error: "Bad Request: passwords do not match"}, status: 400
+  def status_400
+    if params[:password] == ""
+      render json: {error: "Bad Request: password cannot be blank"}, status: 400
+    elsif params[:password] != params[:password_confirmation]
+      render json: {error: "Bad Request: passwords do not match"}, status: 400
+    elsif params[:email] == ""
+      render json: {error: "Bad Request: email cannot be blank"}, status: 400
+    else
+      render json: {error: "Bad Request: email is already taken"}, status: 400
+    end
   end
 
   def status_500
     render json: { error: "Internal Server Error" }, status: 500
-  end
-
-  def status_400
-    render json: { error: "Bad Request: please enter valid parameters"}, status: 400
   end
 end
--- spec/api/v1/users_spec.rb
+++ spec/api/v1/users_spec.rb
@@ -29,12 +29,69 @@ describe "Users API" do
       headers = {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
 
       post "/api/v1/users", headers: headers, params: body.to_json
-      created_user = User.last
 
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
       expect(response.status).to be(400)
+      expect(json[:error]).to eq("Bad Request: passwords do not match")
     end
 
     it "returns an error if the email is already taken" do
+      body1 = {
+        "email": "original@original.com",
+        "password": "password",
+        "password_confirmation": "password"
+        }
+      body2 = {
+        "email": "original@original.com",
+        "password": "password",
+        "password_confirmation": "password"
+        }
+      headers = {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
+
+      post "/api/v1/users", headers: headers, params: body1.to_json
+      post "/api/v1/users", headers: headers, params: body2.to_json
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response.status).to be(400)
+      expect(json[:error]).to eq("Bad Request: email is already taken")
+    end
+
+    it "returns an error if the email field is blank" do
+      body = {
+        "email": "",
+        "password": "password",
+        "password_confirmation": "password"
+        }
+      headers = {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
+
+      post "/api/v1/users", headers: headers, params: body.to_json
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response.status).to be(400)
+      expect(json[:error]).to eq("Bad Request: email cannot be blank")
+    end
+
+    it "returns an error if the email field is blank" do
+      body = {
+        "email": "email@email.email",
+        "password": "",
+        "password_confirmation": "password"
+        }
+      headers = {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
+
+      post "/api/v1/users", headers: headers, params: body.to_json
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response.status).to be(400)
+      expect(json[:error]).to eq("Bad Request: password cannot be blank")
     end
   end
 end
