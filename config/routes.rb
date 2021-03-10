9869200b7aa5a0a37634ec7ffcbc4be24f52c0d1
Liam Cusack <LCusack8108@gmail.com>
2021-03-09 23:54:18 -0700

    Edit route to sessions#create

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
       get '/trails', to: 'trails#index'
       get '/backgrounds', to: 'backgrounds#index'
       post '/users', to: 'users#create'
-      post '/sessions', to: 'sessions#index'
+      post '/sessions', to: 'sessions#create'
     end
   end
 end
