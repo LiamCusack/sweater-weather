b69f559846e2a9ff4d6accb0a2ae12b969c0579b
Liam Cusack <LCusack8108@gmail.com>
2021-03-10 01:07:53 -0700

    Finish session controller methods, sessions spec tests pass, add passing sad path test

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::SessionsController < ApplicationController
   def login
     user = User.find_by(email: params[:email])
+    binding.pry
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: UsersSerializer.new(user)
+    else
+      render json: { error: "Bad Request: Sorry, your credentials are bad"}, status: 400
+    end
   end
 
   private
--- spec/api/v1/sessions_spec.rb
+++ spec/api/v1/sessions_spec.rb
@@ -15,10 +15,10 @@ describe "Sessions API" do
       @user = JSON.parse(response.body, symbolize_names: true)
     end
 
-    it "can create a new session" do
+    it "can login a user" do
       body = {
-        "email": "whatever@example.com",
-        "password": "password"
+        "email": "unique@new.com",
+        "password": "bananas"
         }
       headers = {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
 
@@ -28,8 +28,8 @@ describe "Sessions API" do
       expect(response).to be_successful
       expect(created_session).to be_a(Hash)
       expect(created_session[:data].keys).to eq([:id, :type, :attributes])
-      expect(created_session[:data][:id]).to eq(Session.last.id.to_s)
-      expect(created_session[:data][:type]).to eq("sessions")
+      expect(created_session[:data][:id]).to eq(User.last.id.to_s)
+      expect(created_session[:data][:type]).to eq("users")
       expect(created_session[:data][:attributes]).to be_a(Hash)
       expect(created_session[:data][:attributes].keys).to eq([:email, :api_key])
       expect(created_session[:data][:attributes][:email]).to eq(body[:email])
@@ -37,5 +37,32 @@ describe "Sessions API" do
     end
   end
   describe 'Sad Paths' do
+    before :each do
+      body = {
+        "email": "unique@new.com",
+        "password": "bananas",
+        "password_confirmation": "bananas"
+        }
+      headers = {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
+
+      post "/api/v1/users", headers: headers, params: body.to_json
+
+      @user = JSON.parse(response.body, symbolize_names: true)
+    end
+
+    it "returns an error if password doesnt match" do
+      body = {
+        "email": "unique@new.com",
+        "password": "coconuts"
+        }
+      headers = {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
+
+      post "/api/v1/sessions", headers: headers, params: body.to_json
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response.status).to be(400)
+      expect(json[:error]).to eq("Bad Request: Sorry, your credentials are bad")
+    end
   end
 end
