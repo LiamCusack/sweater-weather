dd48a22e877547f44c4f4a8ede8736e78cfa6ebc
Liam Cusack <LCusack8108@gmail.com>
2021-03-10 06:41:36 -0700

    Add extra sad path testing, coverage at 100%

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,9 +1,13 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
     user = User.find_by(api_key: roadtrip_params[:api_key])
-    if user
+    if user.present? && params[:origin].present? && params[:destination].present?
       roadtrip = RoadTripFacade.create_road_trip(params[:origin], params[:destination])
       render json: RoadTripSerializer.new(roadtrip), status: 201
+    elsif (params[:origin] || params[:destination]) == ""
+      self.status_400
+    else
+      self.unauthorized_api_key
     end
   end
 
