6f75daf7ffe377286c008f46960da63e215e5c69
Liam Cusack <LCusack8108@gmail.com>
2021-03-09 17:25:01 -0700

    Changed image service to unspash due to least authorization/data shenanigans

--- app/facades/backgrounds_facade.rb
+++ app/facades/backgrounds_facade.rb
@@ -3,10 +3,11 @@ class BackgroundsFacade
 
     def get_background(location)
       image = find_image(location)
+
     end
 
     def find_image(location)
-      images = FlickrService.retrieve_images(location)
+      images = PexelsService.retrieve_images(location)
     end
 
   end
