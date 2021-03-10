adf97a444dc7b21bc98629a4d7a45168d1201c36
Liam Cusack <LCusack8108@gmail.com>
2021-03-09 20:10:25 -0700

    users_spec test passes, add email confirmation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  validates :password, :confirmation => true
 end
--- spec/api/v1/users_spec.rb
+++ spec/api/v1/users_spec.rb
@@ -2,20 +2,22 @@ require "rails_helper"
 
 describe "Users API" do
   describe "Happy Paths" do
-    it "exposes api that allows user creation" do
-      user_params = ({
+    it "can create a new user" do
+      body = {
         "email": "whatever@example.com",
         "password": "password",
         "password_confirmation": "password"
-        })
+        }
       headers = {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
 
-      post "/api/v1/users", headers: headers, params: JSON.generate(user: user_params, symbolize_names: true)
+      post "/api/v1/users", headers: headers, params: body.to_json
+      created_user = User.last
+      binding.pry
 
       expect(response).to be_successful
-      expect(created_user.email).to eq(user_params[:email])
-      expect(created_user.password).to eq(user_params[:password])
-      expect(created_user.password_confirmation).to eq(user_params[:password_confirmation])
+      expect(created_user.email).to eq(body[:email])
+      expect(created_user.password).to eq(body[:password])
+      expect(created_user.password).to eq(body[:password_confirmation])
     end
   end
 end
