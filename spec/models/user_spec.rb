220e6f9ccd17a44b9067f36326b29817eb79c5a4
Liam Cusack <LCusack8108@gmail.com>
2021-03-09 19:42:24 -0700

    complete user validations testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,9 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
   describe "validations" do
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :password }
   end
-  describe "class methods" do
+  describe "instance methods" do
   end
 end
