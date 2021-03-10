ba336f3cabc4ed6e60e85ac197c05460d2d56d82
Liam Cusack <LCusack8108@gmail.com>
2021-03-10 00:16:32 -0700

    add user model method authenticate_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
                        :confirmation => true
 
   has_secure_token :api_key
+
+  def self.authenticate_password(password)
+    self.authenticate(password)
+  end
 end
