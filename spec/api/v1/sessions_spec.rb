9dcfc6a4c710044dddf5e2990e8c5182e2d3a830
Liam Cusack <LCusack8108@gmail.com>
2021-03-09 23:47:42 -0700

    Created sessions_spec.rb and create session test

---
+++ spec/api/v1/sessions_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+describe "Sessions API" do
+  describe "Happy Paths" do
+    it "can create a new session" do
+      body = {
+        "email": "whatever@example.com",
+        "password": "password"
+        }
+      headers = {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
+
+      post "/api/v1/sessions", headers: headers, params: body.to_json
+      created_session = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(created_session).to be_a(Hash)
+      expect(created_session[:data].keys).to eq([:id, :type, :attributes])
+      expect(created_session[:data][:id]).to eq(Session.last.id.to_s)
+      expect(created_session[:data][:type]).to eq("sessions")
+      expect(created_session[:data][:attributes]).to be_a(Hash)
+      expect(created_session[:data][:attributes].keys).to eq([:email, :api_key])
+      expect(created_session[:data][:attributes][:email]).to eq(body[:email])
+      expect(created_session[:data][:attributes][:api_key]).to be_a(String)
+    end
+  end
+  describe 'Sad Paths' do
+  end
+end
