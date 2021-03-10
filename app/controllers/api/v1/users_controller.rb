a3260854d127e720c0664702d018cc659bf453f4
Liam Cusack <LCusack8108@gmail.com>
2021-03-10 06:45:51 -0700

    fix typo

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
   def create
       user = User.new(user_params)
       if user.save
-        render json: UsersSerializer.new(user)
+        render json: UsersSerializer.new(user), status: 201
       else
         self.status_400
     end
